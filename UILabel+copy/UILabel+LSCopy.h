cb9c7bc8db1cf865145a38873020d1a17948d839
liangsenliangsen <1096359518@qq.com>
2019-11-13 17:18:54 +0800

    具有拷贝功能

---
+++ UILabel+copy/UILabel+LSCopy.h
@@ -0,0 +1,21 @@
+//
+//  UILabel+LSCopy.h
+//  woi
+//
+//  Created by 中创 on 2019/11/13.
+//  Copyright © 2019 中创-慧谷. All rights reserved.
+//
+
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UILabel (LSCopy)
+
+/// 是否具备拷贝功能
+@property (nonatomic,assign) BOOL isCopyable;
+
+@end
+
+NS_ASSUME_NONNULL_END
